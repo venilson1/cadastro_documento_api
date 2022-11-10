58389ac121c65fab16a557909057d8b540ef4d82
venilson1 <venilson2009@hotmail.com>
2022-11-10 18:12:55 -0300

    feat(documento): definicao interfacerepository

---
+++ Source/Core/Interfaces/Repositories/IDocumentoRepository.cs
@@ -0,0 +1,12 @@
+﻿using cadastro_documento_api.Source.Core.Entities;
+
+namespace cadastro_documento_api.Source.Core.Interfaces.Repositories
+{
+    public interface IDocumentoRepository
+    {
+        Task<List<DocumentoEntity>> FindAll(int page);
+        Task<DocumentoEntity> FindById(int id);
+        Task<DocumentoEntity> Create(DocumentoEntity doc);
+        Task<int> CountPage();
+    }
+}
