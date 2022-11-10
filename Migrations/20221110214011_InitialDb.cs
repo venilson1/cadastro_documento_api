0cc47bf335c0c0b19bc34523566eaa16703c95c4
venilson1 <venilson2009@hotmail.com>
2022-11-10 18:43:45 -0300

    feat: migrations iniciais

---
+++ Migrations/20221110214011_InitialDb.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace cadastro_documento_api.Migrations
+{
+    public partial class InitialDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Processo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    CriadoEm = table.Column<DateTime>(type: "datetime(6)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Processo", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Documento",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Codigo = table.Column<int>(type: "int", nullable: false),
+                    Titulo = table.Column<int>(type: "int", nullable: false),
+                    Categoria = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Arquivo = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    CriadoEm = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    ProcessoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Documento", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Documento_Processo_ProcessoId",
+                        column: x => x.ProcessoId,
+                        principalTable: "Processo",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Processo",
+                columns: new[] { "Id", "CriadoEm", "Nome" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2022, 11, 10, 18, 40, 11, 727, DateTimeKind.Local).AddTicks(7292), "A" },
+                    { 2, new DateTime(2022, 11, 10, 18, 40, 11, 727, DateTimeKind.Local).AddTicks(7305), "B" },
+                    { 3, new DateTime(2022, 11, 10, 18, 40, 11, 727, DateTimeKind.Local).AddTicks(7307), "C" },
+                    { 4, new DateTime(2022, 11, 10, 18, 40, 11, 727, DateTimeKind.Local).AddTicks(7307), "D" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Documento_ProcessoId",
+                table: "Documento",
+                column: "ProcessoId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Documento");
+
+            migrationBuilder.DropTable(
+                name: "Processo");
+        }
+    }
+}
