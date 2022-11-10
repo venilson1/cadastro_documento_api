ed2f7c512f778d9eb30cd40962c53b9961d59db7
venilson1 <venilson2009@hotmail.com>
2022-11-10 18:18:46 -0300

    feat(documento): criacao para dto

---
+++ Source/Core/DTOs/DocumentoDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace cadastro_documento_api.Source.Core.DTOs
+{
+    public class DocumentoDTO
+    {
+        [Required(ErrorMessage = "O Campo Código é obrigatório")]
+        [RegularExpression("([0-9]+)", ErrorMessage = "O Campo Código é somento números")]
+        public int Codigo { get; set; }
+        [Required(ErrorMessage = "O Campo Processo é obrigatório")]
+        public int ProcessoId { get; set; }
+        [Required(ErrorMessage = "O Campo Título é obrigatório")]
+        public int Titulo { get; set; }
+        [Required(ErrorMessage = "O Campo Categoria é obrigatório")]
+        public string Categoria { get; set; }
+        [Required(ErrorMessage = "O Campo Arquivo é obrigatório")]
+        public string Arquivo { get; set; }
+    }
+}
