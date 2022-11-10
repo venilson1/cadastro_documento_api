69a381e4f3310bf30e02b4cb8a12ac2da5227faa
venilson1 <venilson2009@hotmail.com>
2022-11-10 18:46:19 -0300

    feat(documento): implemetacao dos metodos do repository

--- Source/Infraestructure/Repositories/DocumentoRepository.cs
+++ Source/Infraestructure/Repositories/DocumentoRepository.cs
@@ -1,28 +1,44 @@
 ﻿using cadastro_documento_api.Source.Core.Entities;
 using cadastro_documento_api.Source.Core.Interfaces.Repositories;
+using cadastro_documento_api.Source.Infraestructure.Contexts;
+using Microsoft.EntityFrameworkCore;
 
 namespace cadastro_documento_api.Source.Infraestructure.Repositories
 {
     public class DocumentoRepository : IDocumentoRepository
     {
-        public Task<int> CountPage()
+        private readonly CadastroDocumentosContex _dbContex;
+
+        public DocumentoRepository(CadastroDocumentosContex dbContex)
         {
-            throw new NotImplementedException();
+            _dbContex = dbContex;
         }
 
-        public Task<DocumentoEntity> Create(DocumentoEntity doc)
+        public async Task<int> CountPage()
         {
-            throw new NotImplementedException();
+            int total = _dbContex.Documento.Count();
+
+            decimal totalPage = total / 8;
+
+            totalPage = Math.Ceiling(totalPage);
+            return (int)totalPage;
         }
 
-        public Task<List<DocumentoEntity>> FindAll(int page)
+        public async Task<DocumentoEntity> Create(DocumentoEntity doc)
         {
-            throw new NotImplementedException();
+            await _dbContex.Documento.AddAsync(doc);
+            await _dbContex.SaveChangesAsync();
+            return doc;
         }
 
-        public Task<DocumentoEntity> FindById(int id)
+        public async Task<List<DocumentoEntity>> FindAll(int page)
         {
-            throw new NotImplementedException();
+            return await _dbContex.Documento.OrderBy(x => x.Titulo).AsNoTracking().Skip((page * 8)).Take(8).ToListAsync();
+        }
+
+        public async Task<DocumentoEntity> FindById(int id)
+        {
+            return await _dbContex.Documento.FirstOrDefaultAsync(x => x.Id == id);
         }
     }
 }
