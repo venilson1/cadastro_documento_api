0f0cadb7450199055c8d8ae935dd7f8945efb365
venilson1 <venilson2009@hotmail.com>
2022-11-10 18:11:35 -0300

    feat: definicao de entidades

---
+++ Source/Core/Entities/DocumentoEntity.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace cadastro_documento_api.Source.Core.Entities
+{
+    public class DocumentoEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Codigo { get; set; }
+        public int Titulo { get; set; }
+        public string Categoria { get; set; }
+        public string Arquivo { get; set; }
+        public DateTime CriadoEm { get; set; }
+        public int ProcessoId { get; set; }
+        public virtual ProcessoEntity Processo { get; set; }
+    }
+}
---
+++ Source/Core/Entities/ProcessoEntity.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace cadastro_documento_api.Source.Core.Entities
+{
+    public class ProcessoEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime CriadoEm { get; set; }
+        public DocumentoEntity Documento { get; set; }
+    }
+}
