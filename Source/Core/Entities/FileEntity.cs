85d833ce6e02408b5fa96d27176d99e6efd1cac2
venilson1 <venilson2009@hotmail.com>
2022-11-11 22:02:42 -0300

    feat(File): criacao modulo de Arquivo

---
+++ Source/Core/Entities/FileEntity.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace cadastro_documento_api.Source.Core.Entities
+{
+    public class FileEntity
+    {
+        public FileEntity()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        [Key]
+        [Required]
+        public Guid Id { get; set; }
+        public byte[] FileBytes { get; set; }
+        public string ContentType { get; set; }
+        public string FileName { get; set; }
+        [JsonIgnore]
+        public virtual DocumentoEntity Documeto { get; set; }
+
+    }
+}
---
+++ Source/Core/Interfaces/Repositories/IFileRepository.cs
@@ -0,0 +1,10 @@
+﻿using cadastro_documento_api.Source.Core.Entities;
+
+namespace cadastro_documento_api.Source.Core.Interfaces.Repositories
+{
+    public interface IFileRepository
+    {
+        Task<FileEntity> Create(FileEntity file);
+        Task<FileEntity> FindById(Guid id);
+    }
+}
